c838fd6c90583655dd32d7f18761d3e9426fe8be
Alejandro <alejandrocapraargentina@gmail.com>
2021-03-02 17:45:05 -0300

    Arreglo un poco

---
+++ 100-ejercicios/1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#26 febrero 2021 
+#alejandro capra
+#1. Listar todos los archivos del directorio bin.
+
+
+listarArchivosBin=$(ls /bin)
+echo "$listarArchivosBin"
+
+
---
+++ ejercicios-random/directorio-existe.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#26 febrero 2021 
+#alejandro capra
+#Ejercicio 1
+#Mirar si el directorio pasado por parametro ya existe si es si, que muestre un mensaje. 
+#Si es no lo debe crear y decir que lo ha creado
+
+
+read -p "Buscar directorio : " directorio  
+buscarDirectorio=$(find /home -d -name $directorio)
+echo $buscarDirectorio
+
+
